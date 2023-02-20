3b32eac96d1753cd0ce271fae7df2f1acdee7556
Aayush2104 <aayush.2104@gmail.com>
2023-02-21 01:28:28 +0530

    Bau majori kari

--- src/pages/Download.js
+++ src/pages/Download.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import "./download.css"
 
 const onButtonClick = () => {
     fetch('GXPbrochure.pdf').then(response => {
@@ -15,8 +16,29 @@ export default class Download extends Component {
   render() {
     return (
         <>
-        <div className='Contents' style={{marginBottom:500,marginTop:10}}><h1 style={{marginLeft:350}}>Download</h1></div>
-        <button class="button-62" onClick={onButtonClick}>Download GXP Solution brochure</button>
+            
+        <div className='Contents' style={{marginBottom:200,marginTop:10}}><h1 style={{marginLeft:350}}>Download</h1></div>
+        <button class="button-30" onClick={onButtonClick}>Download GXP Solution brochure</button>
+        <div class="form">
+      <div class="title">Welcome</div>
+      <div class="subtitle">Let's create your account!</div>
+      <div class="input-container ic1">
+        <input id="firstname" class="input" type="text" placeholder=" " />
+        <div class="cut"></div>
+        <label for="firstname" class="placeholder">First name</label>
+      </div>
+      <div class="input-container ic2">
+        <input id="lastname" class="input" type="text" placeholder=" " />
+        <div class="cut"></div>
+        <label for="lastname" class="placeholder">Last name</label>
+      </div>
+      <div class="input-container ic2">
+        <input id="email" class="input" type="text" placeholder=" " />
+        <div class="cut cut-short"></div>
+        <label for="email" class="placeholder">Email</label>
+      </div>
+      <button type="text" class="submit">submit</button>
+    </div>
         </>
     )
   }
